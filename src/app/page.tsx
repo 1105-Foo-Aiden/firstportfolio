b0228a68bfc1cb0f472bf0856c5c1631aee2a406
Aiden Foo <aidenfoo154@gmail.com>
2024-05-04 17:45:56 -0700

    rough ROUGH DRAFT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,100 @@
 import Image from "next/image";
+import HeadShot from "./Assets/Headshot.jpg"
+import WeatherApp from "./Assets/WeatherApp.png"
+import Link from "next/link";
+import PokeApi from "./Assets/PokeApi.png"
+import CourtMonitor from "./Assets/Courtmonitor.png"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    <div className="min-h-screen text-center dark:bg-[#000435] bg-green-600 text-white pt-5">
+      <div className="grid grid-cols-2">
+        <div>
+          <h1 className="text-4xl">Aiden Foo</h1>
+          <br/>
+          <p >Aspiring Web Developer based in Stockton CA and Always wanting to learn more</p>
+        </div>
+        <div className="flex justify-end pr-24">
+          <Image src={HeadShot} alt="Headshot" width={200} height={200}></Image>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <br />
+      <br />
+      <p className="text-3xl flex justify-start indent-10">Projects</p>
+      <hr className="border-1 border-white " />
+      <div className="grid grid-cols-3 pt-5">
+        <div className="grid grid-rows-2">
+          <Link href={"https://weatherreactbuild-eight.vercel.app"} target="blank" className="flex justify-center">
+          <Image src={WeatherApp} alt="Weather App" height={300} width={400}/>
+          </Link>
+          <p className="text-3xl pt-5 h-fit">React Weather App</p>
+        </div>
+        <div className="grid grid-rows-2">
+          <Link href={"https://fooa-project9-pokiapi.azurewebsites.net"} target="blank" className="flex justify-center">
+            <Image src={PokeApi} alt="Pokemon Api" height={300} width={400}/>
+          </Link>
+          <p className="text-3xl pt-5 h-fit">Pokemon Api</p>
+        </div>
+        <div className="grid grid-rows-2">
+          <Link href={"https://fullstack-frontend-git-kylebranch-jeriahs-projects-b6db9779.vercel.app"} target="blank" className="flex justify-center">
+            <Image src={CourtMonitor} alt="Court Monitor" height={300} width={400}/>
+          </Link>
+          <p className="text-3xl pt-5 h-fit">Court Monitor Role: Backend API and database Building</p>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <br />
+      <br />
+      <p className="text-3xl flex justify-start indent-10">Skills</p>
+      <hr className="border-1 border-white " />
+      <div className=" grid grid-cols-2 pt-8">
+        <div className="text-2xl">
+          Languages:
+          <div>
+            <ul className="grid grid-cols-2">
+              <li>C#</li>
+              <li>Python</li>
+              <li>HTML</li>
+              <li>JS</li>
+              <li>CSS</li>
+              <li>TS</li>
+              <li>SQL</li>
+              <li>TSX</li>
+            </ul>
+          </div>
+        </div>
+        <div className="text-2xl">
+          Frameworks:
+          <ul className="grid grid-cols-2">
+            <li>Dotnet 7</li>
+            <li>React</li>
+            <li>Next JS</li>
+            <li>Tailwind</li>
+            <li>Bootstrap</li>
+            <li>Node</li>
+          </ul>
+        </div>
       </div>
-    </main>
-  );
+      <br />
+      <br />
+      <p className="text-3xl flex justify-start indent-10">Contact Me</p>
+      <hr className="border-1 border-white " />
+      <div className="grid grid-cols-2 pb-24 pt-5">
+        <div className="flex justify-center">
+          <p className="text-3xl"> 
+            <Link href={"https://github.com/1105-Foo-Aiden"} target="blank" className="decoration-none">GitHub</Link>
+            
+          </p>
+          </div>
+        <div className="flex justify-center">
+          <p className="text-3xl"> 
+            <Link href={"https://www.linkedin.com/in/aiden-foo-58ba262bb/"} target="blank" className="decoration-none">LinkedIn</Link>
+            
+          </p>
+          </div>
+
+      </div>
+    </div>
+    </>
+  ); 
 }
