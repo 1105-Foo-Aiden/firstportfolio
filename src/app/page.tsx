5e42204171731340f775da153bedd1d4f856605c
Aiden Foo <aidenfoo154@gmail.com>
2024-05-24 11:38:42 -0700

    making Icons and formatting text under Projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,11 @@ import WeatherApp from "./Assets/WeatherApp.png"
 import Link from "next/link";
 import PokeApi from "./Assets/PokeApi.png"
 import CourtMonitor from "./Assets/Courtmonitor.png"
-import LinkedIn from "@/app/Assets/linkedin.svg"
-
+import FlowbiteLogo from "./Assets/image.png"
 
 export default function Home() {
   return (
     <>
-
     <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
     <div className="min-h-screen text-center bg-[#151515] text-[#EEEEEE] p-10">
       <div className="grid grid-cols-2">
@@ -22,30 +20,43 @@ export default function Home() {
         <div className="flex justify-end pr-24">
           <Image src={HeadShot} alt="Headshot" className="rounded-md" width={200} height={200}></Image>
         </div>
-        <a href="./Resume.pdf" className="decoration-none text-2lg" target="blank">Find out more on my Resume</a>
+        <a href="./Resume.pdf" className="decoration-none text-2lg w-fit flex items-center" target="blank">Find out more on my Resume</a>
       </div>
       <br />
       <br />
       <p className="text-3xl flex justify-start indent-10">Projects</p>
-      <hr className="border-1 border-white " />
+      <hr className="border-1 border-white" />
       <div className="grid grid-cols-3 pt-10 h-fit">
-        <div className="grid grid-rows-2 h-fit w-96 ">
+        <div className="grid grid-flow-row h-fit w-fill">
           <Link href={"https://weatherreactbuild-eight.vercel.app"} target="blank" className="flex justify-center">
           <Image src={WeatherApp} alt="Weather App" className="rounded-md" height={300} width={400}/>
           </Link>
-          <p className="text-3xl pt-5 h-fit">React Weather App</p>
+          <pre className="text-3xl pt-5 h-fit">React Weather App {'\n'} 
+            <i className="text-6xl devicon-react-original"/>{'\n'}
+            Displaying data in React
+          </pre>
         </div>
-        <div className="grid grid-rows-2 h-fit w-96">
+        <div className="grid grid-rows-2 h-fit w-fill">
           <Link href={"https://fooa-project9-pokiapi.azurewebsites.net"} target="blank" className="flex justify-center">
             <Image src={PokeApi} alt="Pokemon Api" className="rounded-md" height={300} width={400}/>
           </Link>
-          <p className="text-3xl pt-5 h-fit">Pokemon Api</p>
+          <pre className="text-3xl pt-5 h-fit w-fit whitespace-pre-wrap">
+            Pokemon Api {'\n'}
+            <span className="flex flex-row ml-36 gap-3">
+              <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
+              <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
+              <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
+            </span>
+            calling from Apis and displaying the data</pre>
         </div>
-        <div className="grid grid-rows-2 h-fit w-96">
+        <div className="grid grid-rows-2 h-fit w-fill">
           <Link href={"https://fullstack-frontend-git-kylebranch-jeriahs-projects-b6db9779.vercel.app"} target="blank" className="flex justify-center">
             <Image src={CourtMonitor} alt="Court Monitor" className="rounded-md" height={300} width={400}/>
           </Link>
-          <p className="text-3xl pt-5 h-fit">Court Monitor</p>
+          <pre className="text-3xl pt-5 h-fit">Court Monitor {'\n'} 
+          <img className="w-16 flex justify-center ml-52" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" />
+            Solo Backend Dev
+          </pre>
         </div>
       </div>
       <br />
@@ -56,12 +67,9 @@ export default function Home() {
         <div className="text-2xl">
           Languages:
           <div> 
-            <ul className="flex flex-row  gap-4 justify-center mt-5">
+            <ul className="flex flex-row gap-4 justify-center mt-5">
               <li>
                 <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" />
-              </li>
-              <li>
-                <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" />
               </li>
               <li>
                 <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" />
@@ -84,22 +92,14 @@ export default function Home() {
               <li>
                 <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
               </li>
-              
-              <li>
-                <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
-              </li>
-          
             </ul>
           </div>
         </div>
         <div className="text-2xl">
           Frameworks:
-          <ul className="flex row-span-1 gap-4 justify-center mt-5">
+          <ul className="flex flex-row gap-4 justify-center mt-5">
             <li>
               <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dot-net/dot-net-original.svg" />
-            </li>
-            <li>
-              <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
             </li>
             <li>
               <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" />
@@ -117,7 +117,7 @@ export default function Home() {
         </div>
         <div className="text-2xl">
         Technologies:
-        <ul className="flex row-span-1 gap-4 justify-center mt-5">
+        <ul className="flex flex-row gap-4 justify-center mt-5">
           <li>
             <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/reactbootstrap/reactbootstrap-original.svg" />
           </li>
@@ -143,10 +143,26 @@ export default function Home() {
             <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg"/>
           </li>
           <li>
-            <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vercel/vercel-original.svg"/>
+            <i className="text-5xl devicon-vercel-original"></i>
           </li>
-          <li></li>
+          <li>
+            <i className="text-5xl devicon-github-original"></i>
+          </li>
+          <li>
+            <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/androidstudio/androidstudio-original.svg" />
+          </li>
+          <li>
+            <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" />
+          </li>
         </ul>
+        </div>
+        <div className="text-2xl">
+          Libraries:
+          <ul className="flex flex-row gap-4 justify-center mt-5">
+            <li>
+              <img className="w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+            </li>
+          </ul>
         </div>
       </div>
       <br />
